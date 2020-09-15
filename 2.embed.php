1cff3cf413687627b00ca768388650e990e553ee
FrankRemmy <princeugo.remix@gmail.com>
2020-09-15 14:11:20 -0700

    Initial commit

---
+++ 2.embed.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My first hello world</title>
+</head>
+<body>
+    <?php
+        $title = 'HELLO THERE!';
+    ?>
+
+    <!-- This is a function that echoes the title of the page -->
+    <h1><?php echo $title; ?></h1>
+</body>
+</html> 
